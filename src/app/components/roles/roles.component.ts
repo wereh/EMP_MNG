aeb879e3cabede90a4994a8ddd27dceacbe12231
wereh <wereh.manuh@gmail.com>
2024-12-05 11:12:33 +0300

    Data binging

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+
+  firstName: string = 'Angular tutorial';
+  angularVersion = 'Version 18';
+  version: number = 18;
+  isActive: boolean = false;
+  currentDate: Date = new Date();
+  inputType: string = 'radio';
+  selectedState: string = '';
+  
 
 }
